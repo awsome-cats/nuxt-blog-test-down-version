ebd16e4cbe90c4cb87a22293f86d10c5b4e91fae
awesome-cats <cattwin82@gmail.com>
2020-04-22 10:31:11 +0900

    add PostList component

---
+++ pages/admin/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-page">
+    <section class="new-post">
+      <app-button @click="$router.push('/admin/new-post')">
+        Create Post
+      </app-button>
+    </section>
+
+    <!---- 投稿された記事の表示 --->
+    <section class="existing-posts">
+      <h1>Existing Posts</h1>
+    </section>
+    <!---- 投稿された記事の表示 end--->
+  </div>
+</template>
+
+<script>
+import AppButton from '~/components/UI/AppButton'
+export default {
+  layout: 'admin',
+  components: {
+    AppButton
+  }
+}
+</script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,20 +1,45 @@
 <template>
-  <div class="posts-page">
-    <h1>This is posts page</h1>
+  <div>
+    <div>
+      <h3>This is post page</h3>
+    </div>
+    <div class="posts-page">
+    <section class="post-list">
+      <PostList/>
+    </section>
+  </div>
   </div>
 </template>
 
 <script>
+import PostList from '@/components/Posts/PostList'
 export default {
-
+  components: {
+    PostList
+  }
 }
 </script>
 
 <style scoped>
+  div h3 {
+    text-align: center;
+  }
   .posts-page {
     display:flex;
     justify-content: center;
     align-items: center;
   }
+  .post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
 
+
+  
 </style>
+
+
